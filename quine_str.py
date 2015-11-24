7351f78096d2bfd00be3efeeebe1d08a0bc9805a
Jeffrey Ling <jling@college.harvard.edu>
2015-11-24 02:07:33 -0500

    better quine gen

--- quine_str.py
+++ quine_str.py
@@ -2,10 +2,14 @@ import sys
 import re
 filename = sys.argv[1]
 
+seed = "\\\"asdf\\\""
+
 f = open(filename, 'r')
 s = ''
 for line in f:
     s = s + line.encode("string_escape").replace('"', r'\"')
 
+s = s.replace(r"\'", "'").replace('%', '%%')
+s = s.replace(seed, "%#v")
 print s
 
